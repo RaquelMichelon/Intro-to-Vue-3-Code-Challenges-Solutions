e8875cb8d144697296e1bb0a8c11fba4c2f8d647
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-24 11:25:18 +0100

    Code Challenge L3

---
+++ LESSON3/main.js
@@ -0,0 +1,15 @@
+//Code Challenge L3: Attribute Binding
+//Add a url to the data object
+//Bind the url to an anchor tag’s href attribute
+
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            image: './assets/images/socks_green.jpg',
+            // solution
+            url: 'https://www.google.com/'
+            // solution
+        }
+    }
+})
--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 
 - Lesson 2 - Creating the Vue App
     - Code Challenge L2: Add a product description to the data object and display the description using an expression in a p tag.
+
+- Lesson 3 - Attribute Binding
+    - Code Challenge L3: Add a url to the data object and Bind the url to an anchor tag’s href attribute.
