dc5f3292062d5e5f0fbb0c424996916c68a1d17d
Raquel Michelon <69147526+RaquelMichelon@users.noreply.github.com>
2022-08-25 09:36:04 +0100

    inserted comments about lesson 2

--- LESSON2/main.js
+++ LESSON2/main.js
@@ -2,7 +2,11 @@
 //Add a product description to the data object
 //Display the description using an expression in a p tag
 
+
+//heart of a vue app
+//as argument of creatApp we pass an Options Object
 const app = Vue.createApp({
+    //data: function() {
     data() {
         return {
             product: 'Socks',
